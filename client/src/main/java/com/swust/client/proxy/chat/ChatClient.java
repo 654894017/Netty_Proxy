5a9f5178c6c84fd84e65de388df6b57f77b3b5b0
Liu Mingyao <mrliu943903861@163.com>
2020-07-13 23:58:36 +0800

    feature : 初始化

---
+++ client/src/main/java/com/swust/client/proxy/chat/ChatClient.java
@@ -0,0 +1,8 @@
+package com.swust.client.proxy.chat;
+
+/**
+ * @author : MrLawrenc
+ * date  2020/7/13 23:56
+ */
+public class ChatClient {
+}
---
+++ server/src/main/java/com/swust/server/chat/ChatServer.java
@@ -0,0 +1,8 @@
+package com.swust.server.chat;
+
+/**
+ * @author : MrLawrenc
+ * date  2020/7/13 23:57
+ */
+public class ChatServer {
+}
